b200ad86b8b8e83169c31a9d7e766a294e657169
AlexG345 <alexgernez1@gmail.com>
2026-04-30 06:57:46 +0200

    collision stuff

--- src/collision.py
+++ src/collision.py
@@ -1,5 +1,50 @@
+from enum import IntFlag, auto
 from util import math2
 
+class CollisionGroup(IntFlag):
+    PLAYER	= auto()	# 1
+    ENEMY	= auto()	# 2
+
+
+class CollisionRule():
+
+	def __init__(self, *mask_cgs):
+
+		self.mask = 0
+		for mask_cg in mask_cgs:
+			mask |= mask_cg
+
+		self.entities = set()
+
+	def add_entity(self, entity):
+		self.entities.add(entity)
+
+	def remove_entity(self, entity):
+		self.entities.remove(entity)
+
+
+class CollisionHandler():
+
+	def __init__(self):
+		self.rules = {}
+
+	def associate_rule(self, cg: CollisionGroup, cr: CollisionRule):
+		self.rules[cg] = cr
+
+	def tick(self):
+
+		num_rules = len(self.rules)
+
+		for i in range(num_rules):
+			for j in range(i, num_rules):
+				rule1 = self.rules[i]
+				rule2 = self.rules[j]
+
+				if rule1.mask & rule2.layer:
+					pass
+
+				if rule2.mask & rule1.layer:
+					pass
 
 class Hitbox():
 
@@ -15,52 +60,3 @@ class Hitbox():
 
 	def collide(self, hitbox):
 		return math2.circle_to_circle_2p(self.get_pos(), self.get_radius(), hitbox.get_pos(), hitbox.get_radius())
-
-
-class CollisionHandler():
-
-	def __init__(self, groups = {}):
-		self.groups = groups
-
-	def tick(self):
-		# an ENTITY can ONLY have ONE collision group
-
-		#print("---------------")
-
-		for k1 in self.groups:
-			cg1 = self.groups[k1]
-			for k2 in self.groups:
-				cg2 = self.groups[k2]
-
-				#print(k1, len(cg1.entities), k2, len(cg2.entities))
-
-				if not (cg2 in cg1.mask and cg1 in cg2.mask):
-					continue
-
-				#print(" collides")
-
-				for ent1 in cg1.entities:
-					for ent2 in cg2.entities:
-
-						# TODO: save ent1 and ent2 collision result in case it's used again?
-						if not ent1.hitbox.collide(ent2.hitbox):
-							continue
-
-						ent1.health.change(-1)
-						ent2.health.change(-1)
-
-class CollisionGroup():
-
-	def __init__(self):
-		self.mask = set()
-		self.entities = set()
-
-	# mask: { colgroup1, colgroup2, etc... }
-	def set_mask(self, mask):
-		self.mask = mask
-
-	def add_entity(self, entity):
-		self.entities.add(entity)
-
-	def remove_entity(self, entity):
-		self.entities.remove(entity)
